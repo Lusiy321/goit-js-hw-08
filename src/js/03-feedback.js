52a7d57fb052e1f81daea22e77669ec6490adb7b
Volodymyr Lysenko <lusiy321@gmail.com>
2022-11-05 15:26:19 +0200

    Finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+
+const feedback = 'feedback-form-state';
+form.addEventListener('input', throttle(feedbackFoo, 500));
+form.addEventListener('submit', submit);
+
+updateData();
+const data = {};
+
+function updateData() {
+  if (localStorage.getItem(feedback)) {
+    const data = JSON.parse(localStorage.getItem(feedback) || '');
+    form.elements.email.value = data.email;
+    form.elements.message.value = data.message;
+  }
+}
+
+function feedbackFoo() {
+  data.email = form.elements.email.value;
+  data.message = form.elements.message.value;
+  localStorage.setItem(feedback, JSON.stringify(data));
+}
+
+function submit(evt) {
+  evt.preventDefault();
+  console.log(data);
+  evt.currentTarget.reset();
+  localStorage.clear();
+}
